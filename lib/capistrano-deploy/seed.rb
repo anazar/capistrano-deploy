f6dfac1ff431846fecfab3a4abef5408eab2ee3f
cj <cjlazell@googlemail.com>
2012-05-16 15:07:45 -0500

    added deploy:seed

---
+++ lib/capistrano-deploy/seed.rb
@@ -0,0 +1,22 @@
+module CapistranoDeploy
+  module Seed
+    def self.load_into(configuration)
+      configuration.load do
+        set :rake do
+          if using_recipe?(:bundle)
+            'bundle exec rake'
+          else
+            'rake'
+          end
+        end
+
+        namespace :deploy do
+          desc 'seed the database'
+          task :seed do
+            run "cd #{deploy_to} && RAILS_ENV=#{rails_env} #{rake} db:seed" unless 'production' == rails_env
+          end
+        end
+      end
+    end
+  end
+end
