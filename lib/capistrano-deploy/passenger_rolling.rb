a3ff5ccc2aafe425fd4fa0b4973a4e8f26c8c304
Adam Nazar <anazar@gmail.com>
2013-02-04 22:50:29 -0800

    changed to logger.info

--- lib/capistrano-deploy/passenger_rolling.rb
+++ lib/capistrano-deploy/passenger_rolling.rb
@@ -7,7 +7,7 @@ module CapistranoDeploy
           task :restart, :except => { :no_release => true }, :once => true do
             find_servers(:roles => :app).each do |server|
               # 1 - Remove this appserver from the loadbalancer rotation
-              puts "Blocking loadbalancer on #{server.host}"
+              logger.info "Blocking loadbalancer on #{server.host}"
               run "sudo mv /etc/httpd/conf.d/subro.acdcorp.com.conf /etc/httpd/conf.d/subro.acdcorp.com.conf.disabled", :hosts => server.host
               run "sudo mv /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf.disabled /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf", :hosts => server.host
               run "sudo /etc/init.d/httpd graceful", :hosts => server.host
@@ -15,17 +15,17 @@ module CapistranoDeploy
               sleep(5)
 
               # 2 - Restart this appserver
-              puts "Waiting for passenger to start on #{server.host}"
+              logger.info "Waiting for passenger to start on #{server.host}"
               run "touch #{deploy_to}/tmp/restart.txt", :hosts => server.host
               run "curl #{curl_url}' -ks > /dev/null", :hosts => server.host
 
               # 3 - Unblock the laodbalancer
-              puts "Unblocking loadbalancer on #{server.host}"
+              logger.info "Unblocking loadbalancer on #{server.host}"
               run "sudo mv /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf.disabled", :hosts => server.host
               run "sudo mv /etc/httpd/conf.d/subro.acdcorp.com.conf.disabled /etc/httpd/conf.d/subro.acdcorp.com.conf", :hosts => server.host
               run "sudo /etc/init.d/httpd graceful", :hosts => server.host              
               unless servers.last == server
-                puts "Sleeping for 5 seconds until LB notices #{server.host} is up again"
+                logger.info "Sleeping for 5 seconds until LB notices #{server.host} is up again"
                 sleep(5)
               end
             end
