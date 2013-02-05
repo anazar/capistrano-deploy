e54889354cf619aefc38fbe81614777f5d8a5b85
Adam Nazar <anazar@gmail.com>
2013-02-05 11:30:07 -0800

    only restart server if production

--- lib/capistrano-deploy/passenger_rolling.rb
+++ lib/capistrano-deploy/passenger_rolling.rb
@@ -7,12 +7,14 @@ module CapistranoDeploy
           task :restart, :except => { :no_release => true }, :once => true do
             find_servers(:roles => :app).each do |server|
               # 1 - Remove this appserver from the loadbalancer rotation
-              logger.info "Blocking loadbalancer on #{server.host}"
-              run "sudo mv /etc/httpd/conf.d/subro.acdcorp.com.conf /etc/httpd/conf.d/subro.acdcorp.com.conf.disabled", :hosts => server.host
-              run "sudo mv /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf.disabled /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf", :hosts => server.host
-              run "sudo /etc/init.d/httpd graceful", :hosts => server.host
-              #puts "Sleeping for 90 seconds until LB notices #{server.host} is down"
-              sleep(5)
+              if 'production' == rails_env
+                logger.info "Blocking loadbalancer on #{server.host}"
+                run "sudo mv /etc/httpd/conf.d/subro.acdcorp.com.conf /etc/httpd/conf.d/subro.acdcorp.com.conf.disabled", :hosts => server.host
+                run "sudo mv /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf.disabled /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf", :hosts => server.host
+                run "sudo /etc/init.d/httpd graceful", :hosts => server.host
+                #puts "Sleeping for 5 seconds until LB notices #{server.host} is down"
+                sleep(5)
+              end
 
               # 2 - Restart this appserver
               logger.info "Waiting for passenger to start on #{server.host}"
@@ -20,12 +22,13 @@ module CapistranoDeploy
               run "curl #{curl_url} -ks > /dev/null", :hosts => server.host
 
               # 3 - Unblock the laodbalancer
-              logger.info "Unblocking loadbalancer on #{server.host}"
-              run "sudo mv /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf.disabled", :hosts => server.host
-              run "sudo mv /etc/httpd/conf.d/subro.acdcorp.com.conf.disabled /etc/httpd/conf.d/subro.acdcorp.com.conf", :hosts => server.host
-              run "sudo /etc/init.d/httpd graceful", :hosts => server.host              
-
-              sleep(5)
+              if 'production' == rails_env              
+                logger.info "Unblocking loadbalancer on #{server.host}"
+                run "sudo mv /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf /etc/httpd/conf.d/proxy_subro.acdcorp.com.conf.disabled", :hosts => server.host
+                run "sudo mv /etc/httpd/conf.d/subro.acdcorp.com.conf.disabled /etc/httpd/conf.d/subro.acdcorp.com.conf", :hosts => server.host
+                run "sudo /etc/init.d/httpd graceful", :hosts => server.host 
+                sleep(5)
+              end
 
             end
           end
